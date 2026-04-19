3ad2aec0b2a1a6096dd99eb13cbe5ea8a940c25a
Rohan Bhutada <gopalbhutada2221@gmail.com>
2026-04-19 18:25:10 +0530

    added backend api

--- frontend/car-recommender/src/api.js
+++ frontend/car-recommender/src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/api/cars";
+// const API = "http://localhost:5000/api/cars";
+const API = "https://car-recommender-tt1x.onrender.com/api/cars";
 
 export const filterCars = async (filters) => {
   const res = await axios.post(`${API}/filter`, filters);
